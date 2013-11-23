a4f6ebadb067e41d844da20889af4d849bbf5645
Sam Mullins <sammullins.co.uk@gmail.com>
2013-11-23 15:34:20 +0000

    Added an MVC project

---
+++ Samson.Standard.Mvc/SamsonSurfaceController.cs
@@ -0,0 +1,36 @@
+﻿using Samson.Services;
+using Umbraco.Web.Mvc;
+
+namespace Samson.Standard.Mvc
+{
+    public class SamsonSurfaceController : SurfaceController
+    {
+        public SamsonSurfaceController()
+        {
+            StrongContentService = SamsonContext.Current.StrongContentService;
+            StrongMediaService = SamsonContext.Current.StrongMediaService;
+        }
+
+        public SamsonSurfaceController(IStrongContentService contentService, IStrongMediaService mediaService)
+        {
+            StrongContentService = contentService;
+            StrongMediaService = mediaService;
+        }
+
+        /// <summary>
+        /// Gets or sets the published content service that allows for strong typing.
+        /// </summary>
+        /// <value>
+        /// The strong content service.
+        /// </value>
+        protected IStrongContentService StrongContentService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the published media service that allows for strong typing.
+        /// </summary>
+        /// <value>
+        /// The strong media service.
+        /// </value>
+        protected IStrongMediaService StrongMediaService { get; set; }
+    }
+}
---
+++ Samson.Standard.Mvc/StandardSamsonControllerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Samson.Standard.Mvc
+{
+    public class StandardSamsonControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            try
+            {
+                // Create a controller with the samson services if required.
+                if (typeof(SamsonSurfaceController).IsAssignableFrom(controllerType))
+                {
+                    var contentService = SamsonContext.Current.StrongContentService;
+                    var mediaService = SamsonContext.Current.StrongMediaService;
+
+                    return Activator.CreateInstance(controllerType, contentService, mediaService) as Controller;
+                }
+
+                // Otherwise just use the default constructor.
+                return Activator.CreateInstance(controllerType) as Controller;
+            }
+            catch
+            {
+                throw new MissingMethodException("The controller can't be created as there is no matching constructor.");
+            }
+        }
+    }
+}
